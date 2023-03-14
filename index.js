4dfc07c4ead39ae69db0851abe77fc0d23ce735c
erickariuki <eric.kariuki@student.moringaschool.com>
2023-03-14 11:56:12 +0300

    -completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1= 62
+const num2 = 1
+const num3 = 22
+const num4 = 6
+const multiply = num1*num2
+const random=!(Math.random() * 10) + 1;
+const mod= num3 % num4
+const max = Math.max(1, 5, 10, 15, 20)
